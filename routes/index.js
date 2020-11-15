f37e5755236086204fe8cfbd0fdc5e5c6da61070
Eric Allen <aperic234@gmail.com>
2020-11-14 21:31:59 -0700

    latest

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.post('/rentingForm', function(req, res){
+  // var from = req.body.from;
+  // var to = req.body.to;
+ 
+  res.render('rentingForm', {
+     from:req.body.from,
+     to:  req.body.to
+  });
+});
 module.exports = router;
