0a94c6689caa3a5e8d5b0c7708e4bbae7e03ae11
Eric Allen <aperic234@gmail.com>
2020-11-14 20:57:52 -0700

    changed to be my app

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express()
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:true}))
+app.post("/submit-form", (req,res)=>{
+  //the body stores form variables by there name
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  const var1= req.body.username;
+  res.render('postageform')
+})
