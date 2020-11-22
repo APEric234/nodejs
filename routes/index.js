e0a735f2defe48bd158441615b8c21d934f8cc9f
Eric Allen <aperic234@gmail.com>
2020-11-21 21:11:36 -0700

    moved some inside method

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 const { Client } = require('pg');
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-client.connect();
+
 function getMonsteres(){
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+  client.connect();
 client.query('SELECT * FROM monsters;', (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
