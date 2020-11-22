c2019d6f2f289b9f1ff17ddf66185909a56b09e0
Eric Allen <aperic234@gmail.com>
2020-11-21 21:19:17 -0700

    testing stuff

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,64 @@
 var express = require('express');
-const { Client } = require('pg');
+const {
+  Client
+} = require('pg');
 
+
+
+var router = express.Router();
+
+function calculatePostage(weight, type) {
+  //based off zone 1
+  if (type == "FCSR") {
+    if (weight > 12) {
+      return 5.9;
+    } else if (weight > 8) {
+      return 5.3;
+    } else if (weight > 4) {
+      return 4.6
+    } else {
+      return 3.8;
+
+    }
+  } else if (type == "letterS") {
+    if (weight > 3) {
+      return 1;
+    } else if (weight > 2) {
+      return .85;
+    } else if (weight >= 1) {
+      return .7;
+    } else {
+      return .55;
+
+    }
+  } else if (type == "letterM") {
+    if (weight > 3) {
+      return .95;
+    } else if (weight > 2) {
+      return .8;
+    } else if (weight >= 1) {
+      return .65;
+    } else {
+      return .5;
+    }
+  } else {
+    var baseprice = 1;
+    var increment = .2;
+    var ozx = Math.ceil(weight) - 1;
+    return baseprice + (ozx * increment);
+
+  }
+}
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
+
+});
+router.post('/postageForm', function (req, res) {
+  // var from = req.body.from;
+  // var to = req.body.to;
   const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
@@ -8,77 +66,26 @@ const { Client } = require('pg');
     }
   });
   client.connect();
-client.query('select * from monsters;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-
-var router = express.Router();
-function calculatePostage(weight,type){
-  //based off zone 1
-if(type=="FCSR"){
-  if(weight>12){
-    return 5.9;
-  }else if (weight>8){
-    return 5.3;
-  }else if (weight>4){
-    return 4.6
-}else{
-  return 3.8;
-
-}
-}else if(type=="letterS"){
-  if(weight>3){
-    return 1;
-  }else if (weight>2){
-    return .85;
-  }else if (weight>=1){
-    return .7;
-  }else{
-  return .55;
-
-}
-}else if(type=="letterM"){
-  if(weight>3){
-    return .95;
-  }else if (weight>2){
-    return .8;
-  }else if (weight>=1){
-    return .65;
-  }else{
-  return .5;
-}
-}
-else {
-  var baseprice=1;
-  var increment=.2;
-  var ozx=Math.ceil(weight)-1;
-  return baseprice+(ozx*increment);
-
-}
-}
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-  
-});
-router.post('/postageForm', function(req, res){
-  // var from = req.body.from;
-  // var to = req.body.to;
+  var jim;
+  client.query('select * from monsters;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+      jim=row["name"];
+    }
+    client.end();
+  });
   var names = {
-    "FCSR":"PACKAGE",
-    "letterS":"Letters Stamped",
-    "letterM":"Letters Metered",
-    "envelope":"Envelope"
+    "FCSR": "PACKAGE",
+    "letterS": "Letters Stamped",
+    "letterM": "Letters Metered",
+    "envelope": "Envelope"
   }
-  var priceIn=calculatePostage(req.body.weight,req.body.type);
+  var priceIn = calculatePostage(req.body.weight, req.body.type);
   res.render('postageForm', {
-     type:names[req.body.type],
-     weight:  req.body.weight,
-     price:priceIn
+    type: names[req.body.type],
+    weight: req.body.weight,
+    price: jim
   });
 });
 module.exports = router;
