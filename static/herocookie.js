4517ad242c48f6bb3b0f01f86f1a3b86ea98cde4
Eric Allen <aperic234@gmail.com>
2020-12-15 21:06:53 -0700

    changing how image is set

--- static/herocookie.js
+++ static/herocookie.js
@@ -4,6 +4,26 @@ function setCookie(cname,value){
   d.setTime(d.getTime()+days);
   var expires="expires="+d.toUTCString();
   document.cookie=cname+"="+value+"; "+expires;
-  document.getElementById(cname).src="static/images/"+"hero"+value.trim()+".jpg";
+  
 
+}
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+var x = getCookie("hero");
+if(x){
+  document.getElementById(x).src="static/images/"+"hero"+x.trim()+".jpg";
 }
