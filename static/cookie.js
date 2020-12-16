87801995b401b7fd3e2abe062f443410b28f74a0
Eric Allen <aperic234@gmail.com>
2020-12-15 20:56:44 -0700

    added files fixed reference

--- static/cookie.js
+++ static/cookie.js
@@ -4,6 +4,6 @@ function setCookie(cname,value){
   d.setTime(d.getTime()+days);
   var expires="expires="+d.toUTCString();
   document.cookie=cname+"="+value+"; "+expires;
-  document.getElementById(cname).src=value+".jpg";
+  document.getElementById(cname).src="static/images/"+value+".jpg";
 
 }
