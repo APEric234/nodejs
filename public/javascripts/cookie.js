13580a2305eaa2504d3fb14b48b4a69f16fc2668
Eric Allen <aperic234@gmail.com>
2020-12-15 20:34:17 -0700

    added cookie

--- public/javascripts/cookie.js
+++ public/javascripts/cookie.js
@@ -1,5 +1,8 @@
+function setCookie(cname,value){
+  var days=3*24*60*60*1000;
+  var d = new Date();
+  d.setTime(d.getTime()+days);
+  var expires="expires="+d.toUTCString();
+  document.cookie=cname+"="+value+"; "+expires;
 
-var h_id=a;
-
-
-document.cookie = "hero="+
+}
